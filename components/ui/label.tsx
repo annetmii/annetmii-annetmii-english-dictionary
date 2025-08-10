03e29f6aff8e3d695fbb2ef7b2aaf813d35ee68c
annetmii <i.freezea@gmail.com>
2025-08-11 01:04:26 +0900

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 export function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
   return <label className={`text-sm text-gray-700 ${className||""}`} {...props} />;
