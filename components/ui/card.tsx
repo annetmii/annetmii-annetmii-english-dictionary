b4e31189b3463a5fae895de9297da6f17c55df5d
annetmii <i.freezea@gmail.com>
2025-08-11 01:03:19 +0900

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return <div className={`rounded-2xl border border-gray-200 bg-white shadow-sm ${className||""}`} {...props} />;
