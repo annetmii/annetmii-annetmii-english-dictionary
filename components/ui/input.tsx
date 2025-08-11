b4f1daa6ba5f18250a81e4b8a06a3ed97e085f96
annetmii <i.freezea@gmail.com>
2025-08-11 12:44:27 +0900

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,13 +1,14 @@
 "use client";
 import * as React from "react";
+
 export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, ...props }, ref) => (
     <input
-  ref={ref}
-  className={`w-full border rounded-xl px-3 py-2 text-sm text-gray-900 placeholder:text-gray-400 ${className||""}`}
-  {...props}
-/>
+      ref={ref}
+      className={`w-full border rounded-xl px-3 py-2 text-sm text-gray-900 placeholder:text-gray-400 ${className || ""}`}
+      {...props}
+    />
   )
 );
-Input.displayName="Input";
+Input.displayName = "Input";
 export default Input;
